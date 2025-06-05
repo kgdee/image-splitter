c01f9d3621ea83165773a0e0d13314a3c85c0aff
kgdee <kgdee7603@gmail.com>
2025-06-05 07:44:47 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,58 @@
+window.addEventListener("error", (event) => {
+  const error = `${event.type}: ${event.message}`;
+  console.error(error);
+  alert(error);
+});
+
+async function splitImage(file) {
+  if (!file) return
+
+  const nameParts = file.name.match(/(.*?)(\.[^.]+)$/);
+  const imageFileName = nameParts ? nameParts[1] : "slice";
+  const imageExtension = nameParts ? nameParts[2] : ".png";
+
+  const imageEl = await createImageEl(file);
+
+  const rows = parseInt(document.getElementById("rows").value);
+  const cols = parseInt(document.getElementById("cols").value);
+  const preview = document.getElementById("preview");
+  preview.innerHTML = "";
+
+  const width = imageEl.width;
+  const height = imageEl.height;
+  const sliceWidth = width / cols;
+  const sliceHeight = height / rows;
+
+  let index = 2;
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      canvas.width = sliceWidth;
+      canvas.height = sliceHeight;
+
+      ctx.drawImage(imageEl, col * sliceWidth, row * sliceHeight, sliceWidth, sliceHeight, 0, 0, sliceWidth, sliceHeight);
+
+      const link = document.createElement("a");
+      link.download = `${imageFileName} (${index++})${imageExtension}`;
+      link.href = canvas.toDataURL();
+      link.appendChild(canvas);
+      preview.appendChild(link);
+    }
+  }
+}
+
+async function createImageEl(file) {
+  const dataUrl = await new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (event) => resolve(event.target.result);
+    reader.onerror = reject;
+    reader.readAsDataURL(file);
+  });
+
+  const imageEl = new Image();
+  imageEl.src = dataUrl;
+
+  return imageEl;
+}
